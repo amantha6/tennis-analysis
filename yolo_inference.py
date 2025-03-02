d247490c38f6e60bc1b3eaabc477eb212122aee2
amantha6 <amantha@binghamton.edu>
2025-03-02 13:06:20 -0500

    Video object detection

--- yolo_inference.py
+++ yolo_inference.py
@@ -1,4 +1,8 @@
 from ultralytics import YOLO
 model=YOLO('yolov8x')
 
-model.predict('input_videos/image.png', save=True)
+result = model.predict('/mnt/c/Users/SUNYLoaner/Downloads/tennis-analysis/input_videos/input_video.mp4', save=True)
+print(result)
+print("boxes:")
+for box in result[0].boxes:
+    print(box)
