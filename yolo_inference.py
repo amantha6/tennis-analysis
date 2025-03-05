846c5ea660170f385d75cda5ad4bae766856a32a
Aditi Mantha <143033998+amantha6@users.noreply.github.com>
2025-03-05 12:24:01 -0500

    Add files via upload

--- yolo_inference.py
+++ yolo_inference.py
@@ -1,8 +1,9 @@
-from ultralytics import YOLO
-model=YOLO('yolov8x')
+from ultralytics import YOLO 
 
-result = model.predict('/mnt/c/Users/SUNYLoaner/Downloads/tennis-analysis/input_videos/input_video.mp4', save=True)
-print(result)
-print("boxes:")
-for box in result[0].boxes:
-    print(box)
+model = YOLO('yolov8x')
+
+result = model.track('input_videos/input_video.mp4',conf=0.2, save=True)
+# print(result)
+# print("boxes:")
+# for box in result[0].boxes:
+#     print(box)
